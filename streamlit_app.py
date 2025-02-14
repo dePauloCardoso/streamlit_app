1bfc4c135bb30d494582a06a1179383caf792b9d
Paulo Henrique Cardoso <paulocardoso@arcoeducacao.com.br>
2025-02-14 00:14:32 -0300

    nova atualizacao

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,21 @@ import pandas as pd
 API_URL = "https://api-streamlit-7h4o.onrender.com"
 
 # Função para consultar a API
-def consultar_api(**kwargs):
-    params = {key: value for key, value in kwargs.items() if value}
+def consultar_api(**kwargs):  # Aceita qualquer número de argumentos nomeados
+    params = {}
+    for key, value in kwargs.items():
+        if value:
+            params[key] = value
     response = requests.get(f"{API_URL}/produtos", params=params, verify=False)
-    return response.json() if response.status_code == 200 else None
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
-st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
 # Define a cor de fundo
 st.markdown(
@@ -28,30 +37,43 @@ st.markdown(
 # Exibe a imagem
 st.sidebar.image("https://github.com/dePauloCardoso/streamlit_app/blob/main/Logo_SAE.png?raw=true")
 
+# Interface Streamlit
 st.sidebar.title("Consulta de Produtos")
 
-# Inicializa os estados da sessão para filtros
-if "segmento" not in st.session_state:
-    st.session_state.segmento = ""
-if "serie" not in st.session_state:
-    st.session_state.serie = ""
-if "cod_insersao" not in st.session_state:
-    st.session_state.cod_insersao = ""
-if "cod_sku" not in st.session_state:
-    st.session_state.cod_sku = ""
-if "envio" not in st.session_state:
-    st.session_state.envio = ""
-if "usuario" not in st.session_state:
-    st.session_state.usuario = ""
-if "personalizacao" not in st.session_state:
-    st.session_state.personalizacao = ""
+# Inicializa session_state
+if 'filtros' not in st.session_state:
+    st.session_state.filtros = {
+        "cod_insersao": "",
+        "cod_sku": "",
+        "segmento": "",
+        "serie": "",
+        "envio": "",
+        "usuario": "",
+        "personalizacao": ""
+    }
 
-# Opções de segmento
+# Campos de entrada para os filtros
+def atualiza_filtros():
+    st.session_state.filtros.update({
+        "cod_insersao": cod_insersao,
+        "cod_sku": cod_sku,
+        "segmento": segmento,
+        "serie": serie,
+        "envio": envio,
+        "usuario": usuario,
+        "personalizacao": personalizacao
+    })
+
+    st.experimental_rerun()
+
+cod_insersao = st.sidebar.text_input("Código de Inserção", value=st.session_state.filtros["cod_insersao"], on_change=atualiza_filtros)
+cod_sku = st.sidebar.text_input("Código SKU", value=st.session_state.filtros["cod_sku"], on_change=atualiza_filtros)
+
+# Dropdowns para os filtros
 segmento_options = ["", "INF", "FUND AI", "FUND AF", "EM", "PV", "VÁRIOS"]
+segmento = st.sidebar.selectbox("Segmento", segmento_options, index=segmento_options.index(st.session_state.filtros["segmento"]), on_change=atualiza_filtros)
 
-# Mapeamento de séries por segmento
 serie_dict = {
-    "": [""],
     "INF": ["", "INF I", "INF II", "INF III", "INF IV", "INF V"],
     "FUND AI": ["", "1O ANO", "2O ANO", "3O ANO", "4O ANO", "5O ANO"],
     "FUND AF": ["", "6O ANO", "7O ANO", "8O ANO", "9O ANO"],
@@ -59,50 +81,37 @@ serie_dict = {
     "PV": ["", "APROVA +", "ELETIVAS", "SEMI"],
     "VÁRIOS": ["", "VARIOS"]
 }
+serie_options = serie_dict.get(segmento, [""])
+serie = st.sidebar.selectbox("Série", serie_options, index=serie_options.index(st.session_state.filtros["serie"]), on_change=atualiza_filtros)
 
-# Campos de entrada para os filtros
-st.session_state.cod_insersao = st.sidebar.text_input("Código de Inserção", st.session_state.cod_insersao)
-st.session_state.cod_sku = st.sidebar.text_input("Código SKU", st.session_state.cod_sku)
+envio_options = ["", "V1", "V2", "V3", "V4"]
+envio = st.sidebar.selectbox("Envio", envio_options, index=envio_options.index(st.session_state.filtros["envio"]), on_change=atualiza_filtros)
 
-# Filtros dropdowns
-tmp_segmento = st.sidebar.selectbox("Segmento", segmento_options, index=segmento_options.index(st.session_state.segmento))
-st.session_state.segmento = tmp_segmento
+usuario_options = ["", "Aluno", "Professor"]
+usuario = st.sidebar.selectbox("Usuário", usuario_options, index=usuario_options.index(st.session_state.filtros["usuario"]), on_change=atualiza_filtros)
 
-serie_options = serie_dict.get(st.session_state.segmento, [""])
-tmp_serie = st.sidebar.selectbox("Série", serie_options, index=serie_options.index(st.session_state.serie))
-st.session_state.serie = tmp_serie
-
-st.session_state.envio = st.sidebar.selectbox("Envio", ["", "V1", "V2", "V3", "V4"], index=["", "V1", "V2", "V3", "V4"].index(st.session_state.envio))
-st.session_state.usuario = st.sidebar.selectbox("Usuário", ["", "Aluno", "Professor"], index=["", "Aluno", "Professor"].index(st.session_state.usuario))
-st.session_state.personalizacao = st.sidebar.selectbox("Personalização", ["", "CAMILA MOREIRA", "CCPA", "CELLULA MATER", "DOM BOSCO", "DOM BOSCO BALSAS", "ELO", "FATO", "FILOMENA", "GABARITO MG", "GABARITO RS", "MACK", "MAXX JUNIOR", "MELLO DANTE", "REDE AGNUS", "REDE VIVO", "REFERENCIAL", "ROSALVO", "SAE", "SANTO ANJO", "SECULO", "STATUS", "TAMANDARE"], index=["", "CAMILA MOREIRA", "CCPA", "CELLULA MATER", "DOM BOSCO", "DOM BOSCO BALSAS", "ELO", "FATO", "FILOMENA", "GABARITO MG", "GABARITO RS", "MACK", "MAXX JUNIOR", "MELLO DANTE", "REDE AGNUS", "REDE VIVO", "REFERENCIAL", "ROSALVO", "SAE", "SANTO ANJO", "SECULO", "STATUS", "TAMANDARE"].index(st.session_state.personalizacao))
+personalizacao_options = ["", "CAMILA MOREIRA", "CCPA", "CELLULA MATER","DOM BOSCO", "DOM BOSCO BALSAS", "ELO", "FATO", "FILOMENA", 
+                        "GABARITO MG", "GABARITO RS", "MACK", "MAXX JUNIOR", "MELLO DANTE", "REDE AGNUS", "REDE VIVO", "REFERENCIAL", 
+                        "ROSALVO", "SAE", "SANTO ANJO", "SECULO", "STATUS", "TAMANDARE"]
+personalizacao = st.sidebar.selectbox("Personalização", personalizacao_options, index=personalizacao_options.index(st.session_state.filtros["personalizacao"]), on_change=atualiza_filtros)
 
 # Botão para limpar filtros
-if st.sidebar.button("Limpar Filtros"):
-    for key in ["segmento", "serie", "cod_insersao", "cod_sku", "envio", "usuario", "personalizacao"]:
-        st.session_state[key] = ""
-    st.rerun()
+def limpar_filtros():
+    st.session_state.filtros = {key: "" for key in st.session_state.filtros}
+    st.experimental_rerun()
+st.sidebar.button("Limpar Filtros", on_click=limpar_filtros)
 
-# Botão para consultar
-df = None
-if st.sidebar.button("Consultar"):
-    produtos = consultar_api(
-        cod_insersao=st.session_state.cod_insersao,
-        cod_sku=st.session_state.cod_sku,
-        segmento=st.session_state.segmento,
-        serie=st.session_state.serie,
-        envio=st.session_state.envio,
-        usuario=st.session_state.usuario,
-        personalizacao=st.session_state.personalizacao
-    )
-    if produtos:
-        df = pd.DataFrame(produtos)
-        colunas_desejadas = ['cod_insersao', 'descricao_kit', 'cod_sku',
-                              'descricao_sku', 'segmento', 'serie', 'volume', 'envio',
-                              'frequencia', 'usuario', 'info_produto', 'tipo_material',
-                              'classificacao_produto', 'personalizacao']
-        df = df[colunas_desejadas]
+# Consulta a API com os filtros atuais
+produtos = consultar_api(**st.session_state.filtros)
 
-if df is not None:
+# Exibe os resultados em tabela
+if produtos:
+    df = pd.DataFrame(produtos)
+    colunas_desejadas = ['cod_insersao', 'descricao_kit', 'cod_sku',
+                         'descricao_sku', 'segmento', 'serie', 'volume', 'envio',
+                         'frequencia', 'usuario', 'info_produto', 'tipo_material',
+                         'classificacao_produto', 'personalizacao']
+    df = df[colunas_desejadas]
     st.dataframe(df, use_container_width=True)
 else:
     st.write("Nenhum produto encontrado para estes filtros.")
